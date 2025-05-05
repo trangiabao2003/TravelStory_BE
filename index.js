a59d533154800e5dfa3957acb90e21299a6f80c5
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-05-05 20:34:44 +0700

    deploy

--- index.js
+++ index.js
@@ -354,5 +354,6 @@ app.get("/travel-stories/filter", authenticateToken, async (req, res) => {
 	}
 });
 
-app.listen(8000);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
 module.exports = app;
